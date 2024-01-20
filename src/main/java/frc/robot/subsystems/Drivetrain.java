c1c4fcc4c30803faae9a2556bc0f0bacf9482927
Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>
2024-01-20 13:24:20 -0600

    Created Motors in Swerve Drive

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -11,11 +11,18 @@ public class Drivetrain {
   private static Drivetrain instance;
   private final WPI_PigeonIMU gyro = new WPI_PigeonIMU(Constants.Drivetrain.Gyro);
 
+  private static SwerveModule frontLeft;
+  private static SwerveModule frontRight;
+
+  private static SwerveModule backLeft;
+
+  private static SwerveModule backRight;
+
   public static SwerveDriveKinematics swerveKinematics;
 
   public static SwerveDriveOdometry swerveOdometry;
 
-  Drivetrain() {
+  private Drivetrain() {
     swerveKinematics =
         new SwerveDriveKinematics(
             new Translation2d(Constants.wheelBase / 2, Constants.trackWidth / 2),
@@ -23,6 +30,10 @@ public class Drivetrain {
             new Translation2d(-Constants.wheelBase / 2, Constants.trackWidth / 2),
             new Translation2d(-Constants.wheelBase / 2, -Constants.trackWidth / 2));
     Rotation2d rotation = gyro.getRotation2d();
+    frontLeft = new SwerveModule(Constants.Drivetrain.frontLeftDrivePort, Constants.Drivetrain.frontLeftTurnPort);
+    frontRight = new SwerveModule(Constants.Drivetrain.frontRightDrivePort, Constants.Drivetrain.frontRightTurnPort);
+    backLeft = new SwerveModule(Constants.Drivetrain.backLeftDrivePort, Constants.Drivetrain.backLeftTurnPort);
+    backRight = new SwerveModule(Constants.Drivetrain.backRightDrivePort, Constants.Drivetrain.backRightTurnPort);
     //    swerveOdometry = new SwerveDriveOdometry(swerveKinematics, rotation, new
     // SwerveModulePosition(0.0, rotation));
   }
