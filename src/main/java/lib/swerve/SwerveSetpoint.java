8a47d0fc252c13d6626a140caca4e0909e36bf64
Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>
2024-02-03 15:46:04 -0600

    Rebase on latest main code

---
+++ src/main/java/lib/swerve/SwerveSetpoint.java
@@ -0,0 +1,15 @@
+package lib.swerve;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+public class SwerveSetpoint {
+    public ChassisSpeeds mChassisSpeeds;
+    public SwerveModuleState[] mModuleStates;
+
+    public SwerveSetpoint(ChassisSpeeds chassisSpeeds, SwerveModuleState[] initialStates){
+        this.mChassisSpeeds = chassisSpeeds;
+        this.mModuleStates = initialStates;
+
+    }
+}
