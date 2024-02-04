b5a785309f384cd0ab5af84465ff6824da3e4178
Nova <Froggo8311@proton.me>
2024-02-03 18:52:24 -0600

    Use new numbers for translation2d

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -38,10 +38,10 @@ public class Drivetrain extends SubsystemBase {
     this.gyro.calibrate();
 
     this.swerveKinematics = new SwerveDriveKinematics(
-      new Translation2d(Units.inchesToMeters(-11.875), Units.inchesToMeters(8.625)),  // Front Left
-      new Translation2d(Units.inchesToMeters(11.875), Units.inchesToMeters(8.625)),  // Front Right
-      new Translation2d(Units.inchesToMeters(-11.875), Units.inchesToMeters(-11.125)),  // Back Left
-      new Translation2d(Units.inchesToMeters(11.875), Units.inchesToMeters(-11.125))  // Back Right
+      new Translation2d(Units.inchesToMeters(8.625), Units.inchesToMeters(11.875)),  // Front Left
+      new Translation2d(Units.inchesToMeters(8.625), Units.inchesToMeters(-11.875)),  // Front Right
+      new Translation2d(Units.inchesToMeters(-11.125), Units.inchesToMeters(11.875)),  // Back Left
+      new Translation2d(Units.inchesToMeters(-11.125), Units.inchesToMeters(-11.875))  // Back Right
     );
     this.frontLeft = new SwerveModule(Constants.Ports.FRONT_LEFT_DRIVE, Constants.Ports.FRONT_LEFT_TURN, Constants.Drivetrain.FRONT_LEFT_KENCODER_OFFSET, "front left");
     this.frontRight = new SwerveModule(Constants.Ports.FRONT_RIGHT_DRIVE, Constants.Ports.FRONT_RIGHT_TURN, Constants.Drivetrain.FRONT_RIGHT_KENCODER_OFFSET, "front right");
