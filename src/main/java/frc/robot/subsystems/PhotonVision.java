c191eb3fefe7891cc395ad534f0898f4d02c4aad
Anthony Do <Anthonydo953@gmail.com>
2024-02-10 18:54:48 -0600

    Method to return Pose for robot to go to

--- src/main/java/frc/robot/subsystems/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -38,7 +38,8 @@ public class PhotonVision extends SubsystemBase {
 
   @Override
   public void periodic() {
-
+    String coordinates = "X: " + getRobotPose().get().estimatedPose.getX() + " Y: " + getRobotPose().get().estimatedPose.getY();
+    SmartDashboard.putString("Camera Results", coordinates);
   }
 
   //Gets current pose and timestamp on field using PhotonPoseEstimator
@@ -70,11 +71,33 @@ public class PhotonVision extends SubsystemBase {
 
   //Returns a Pose3d
   public Pose2d getClosestScoringPoint() {
-     double yPoint = Constants.scoringRange * Math.sin(angleFromGoal(getRobotPose().get().estimatedPose).getDegrees());
-     double xPoint = Constants.scoringRange * Math.cos(angleFromGoal(getRobotPose().get().estimatedPose).getDegrees());
+    double xPoint;
+    double yPoint;
+    switch (DriverStation.getAlliance().orElse(DriverStation.Alliance.Red)) {
+      //math will change when angleRelativeToGoal is finished
+      case Blue:
+        xPoint = Constants.scoringRange * Math.cos(angleRelativeToGoal()) * -1;
+        yPoint = 218.42 - Constants.scoringRange * Math.sin(angleRelativeToGoal());
+      case Red:
+        xPoint = 652.73 - Constants.scoringRange * Math.cos(angleRelativeToGoal());
+        yPoint = 218 - Constants.scoringRange * Math.sin(angleRelativeToGoal());
+    }
      return new Pose2d(xPoint, yPoint, getRobotPose().get().estimatedPose.getRotation().toRotation2d());
   }
 
+  //Returns angle relative to the goal regardless of what way the robot is facing
+  public double angleRelativeToGoal() {
+    switch (DriverStation.getAlliance().orElse(DriverStation.Alliance.Red)) {
+      case Red:
+        return Math.asin((getRobotPose().get().estimatedPose.getY() - 218.42) /
+                Math.sqrt(Math.pow(getRobotPose().get().estimatedPose.getY() - 218.42, 2) +
+                        Math.pow(getRobotPose().get().estimatedPose.getX() - 652.73, 2)));
+      case Blue:
+        return Math.asin((getRobotPose().get().estimatedPose.getY() - 218.42) /
+                Math.sqrt(Math.pow(getRobotPose().get().estimatedPose.getY() - 218.42, 2) +
+                        Math.pow(getRobotPose().get().estimatedPose.getX() + 1.5, 2)));
+    }
+  }
 
   //Returns a Transform2d with the distance needed to move(if at all) and the rotation needed to face the goal
   //Can be adjusted to be in certain scoring spots rather than
@@ -82,59 +105,12 @@ public class PhotonVision extends SubsystemBase {
     //if in scoring range assuming that the range has a radius
     if(distanceFromGoal(getRobotPose().get().estimatedPose) <= Constants.scoringRange) {
       //return Transform2d staying in same place and just rotating to line up with goal
-      return new Transform2d(new Translation2d(), angleFromGoal(getRobotPose().get().estimatedPose));
+      return new Transform2d(new Translation2d(), Rotation2d.fromRadians(getRobotPose().get().estimatedPose.getRotation().getZ()));
     } else {
       //hopefully returns a Transform2d that tells robot where to go and how much to rotate by
       return new Transform2d(new Translation2d(Math.abs(getClosestScoringPoint().getX() - getRobotPose().get().estimatedPose.getX()),
-              Math.abs(getClosestScoringPoint().getY() - getRobotPose().get().estimatedPose.getY())), angleFromGoal(getRobotPose().get().estimatedPose));
+              Math.abs(getClosestScoringPoint().getY() - getRobotPose().get().estimatedPose.getY())),
+              Rotation2d.fromRadians(getRobotPose().get().estimatedPose.getRotation().getZ()));
     }
   }
-  /*
-  Just writing down stuff to think about
-  Objectives
-    -Give "instructions" to other subsystems to run (Give coordinates to move to and how to get there)
-      -Can look for potentially better way to interact with other subsystems
-    -Use camera to identify AprilTags and then run some code based on which tag
-    -Specifically look for tags 7 and 8 to run code to get into scoring position and send instructions to other subsystems
-    -Use tag 8 for scoring(8 is lined with in the middle of the speaker)
-      -meaning if robot recognizes tag 7, use that to find position on field to line up with 8 and score
-    -What about amp? Do we want to be able to adjust launcher to shoot into amp?
-      -amp takes 2 notes for 10 second boost
-        -score comparison
-          -2 points for amp + 5 * number of notes in speaker during 10s period: number of points = 2 + 5x
-          -2 * number of notes in speaker + 4 points for the same number of notes it would take for the amp = 2x + 4
-          -Amp seems much better, especially when hit at the right time(when teammates are ready to score)
-    -Do we want the robot to go to a specific spot and then shoot or be in a "scoring range"
-     and adjust angle based on where it is on the field?
-    -Create method for recognizing AprilTags in stage assembly, then give instructions for putting robot in right spot
-     to get onto the chain
-    -Create method for finding out where on field robot is using AprilTags
-      -Needs to take into account
-        -Angle that the robot is recognizing it from
-        -How far robot is from the AprilTag
-          -Could use getArea(), less % = further and just need to test to find out % to distance ratio
-            -If using getArea(), getSkew() needs to be used
-             says "All of the data above (except skew) is available when using AprilTags", so maybe not
-              -Need to find %area to %skew to distance ratio
-              -Need to figure out how to identify where the %area is
-                -To left vs to right of center of vision is two very different positions
-        -Left vs right side of the AprilTag
-          -Can use angle
-          -getSkew() would be best but might not work
-          -Depends on how angle is decided
-            -Left: 180<270 degrees for angle of opposite corner on bottom line of AprilTag 90<180 for top of AprilTag
-            -Right: 270<360 degrees for angle of opposite corner on bottom line 0< 90 for top of AprilTag
-            -Opposite quadrant when using corner closest to us
-          -Can possibly use pythagorean theorem using the corners?
-            -Create a triangle with calculated measurements
-            -Use inverse trig functions to get the angles within the triangle, then 90 - calculated angle?
-    -METHODS that are useful for solving obj above
-      -estimateFieldToRobotAprilTag(Transform3d cameraToTarget, Pose3d fieldRelativeTagPose, Transform3d cameraToRobot)
-       returns your robot’s Pose3d on the field using the pose of the AprilTag relative to the camera,
-       pose of the AprilTag relative to the field, and the transform from the camera to the origin of the robot.
-       ex. Pose3d robotPose = PhotonUtils.estimateFieldToRobotAprilTag(target.getBestCameraToTarget(),
-           aprilTagFieldLayout.getTagPose(target.getFiducialId()), cameraToRobot);
-      -
-   */
-
 }
