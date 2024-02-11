a3678468a46d2a0400f5ab7c90a1d168f603bcc5
Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>
2024-02-10 18:21:53 -0600

    Added phoenix vendor libraries and converted Neo to Kraken, hope it works

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.*;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -10,9 +14,16 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class SwerveModule extends SubsystemBase {
-  private CANSparkMax driveMotor;
+  // Various motors
+  private TalonFX driveMotorLeader;
+  // Allows full use of 15% power
+  private VoltageOut driveMotorRequest;
   private CANSparkMax turnMotor;
+
+  // For logging prettiness
   private String niceName;
+
+  // Used to calculate velocity to voltage
   private SimpleMotorFeedforward feedforward;
 
   private volatile double targetAngle;
@@ -20,15 +31,24 @@ public class SwerveModule extends SubsystemBase {
 
   public SwerveModule(int drivePort, int turnPort, double kencoderOffset, String niceName) {
     this.niceName = niceName;
-    this.driveMotor = new CANSparkMax(drivePort, CANSparkMax.MotorType.kBrushless);
-    this.driveMotor.setInverted(true);
-    this.driveMotor.setSmartCurrentLimit(Constants.Drivetrain.DRIVE_CURRENT_LIMIT);
+
+    // TalonFX doesn't use RIO canbus, it uses its own
+    this.driveMotorLeader = new TalonFX(drivePort, "moe");
+    this.driveMotorLeader.setInverted(true);
+    // Enables FOC (15% extra power)
+    this.driveMotorRequest = new VoltageOut(0).withEnableFOC(true);
+    CurrentLimitsConfigs currentLimits = new CurrentLimitsConfigs().withStatorCurrentLimit(Constants.Drivetrain.DRIVE_CURRENT_LIMIT).withStatorCurrentLimitEnable(true);
+    TalonFXConfiguration driveMotorConfiguration = new TalonFXConfiguration().withCurrentLimits(currentLimits);
+    this.driveMotorLeader.getConfigurator().apply(driveMotorConfiguration);
+
+    // Configures turn motor
     this.turnMotor = new CANSparkMax(turnPort, CANSparkMax.MotorType.kBrushless);
     this.turnMotor.restoreFactoryDefaults();
     this.turnMotor.setInverted(true);
     this.turnMotor.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle).setZeroOffset(kencoderOffset);
     this.turnMotor.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle).setPositionConversionFactor(2 * Math.PI);
     this.turnMotor.setSmartCurrentLimit(Constants.Drivetrain.TURN_CURRENT_LIMIT);
+
     this.feedforward = new SimpleMotorFeedforward(Constants.Drivetrain.MODULE_S, Constants.Drivetrain.MODULE_V, Constants.Drivetrain.MODULE_A);
 
     SparkPIDController turnPID = this.turnMotor.getPIDController();
@@ -51,18 +71,20 @@ public class SwerveModule extends SubsystemBase {
 
   @Override
   public void periodic() {
+    // Sets angles and velocities
     this.turnMotor.getPIDController().setReference(targetAngle, CANSparkBase.ControlType.kPosition);
-    this.driveMotor.setVoltage(feedforward.calculate(targetVelocity));
+    this.driveMotorLeader.setControl(driveMotorRequest.withOutput(feedforward.calculate(targetVelocity)));
 
     SmartDashboard.putNumber(this.niceName + " turn angle", this.getTurnAngle());
     SmartDashboard.putNumber(this.niceName + " turn velocity", this.turnMotor.getEncoder().getVelocity());
     SmartDashboard.putNumber(this.niceName + " turn kencoder position", this.turnMotor.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle).getPosition());
-    SmartDashboard.putNumber(this.niceName + " drive velocity", this.driveMotor.getEncoder().getVelocity());
-    SmartDashboard.putNumber(this.niceName + " drive encoder position", this.driveMotor.getEncoder().getPosition());
+    SmartDashboard.putNumber(this.niceName + " drive velocity", this.driveMotorLeader.getVelocity().getValueAsDouble());
+    SmartDashboard.putNumber(this.niceName + " drive encoder position", this.driveMotorLeader.getPosition().getValueAsDouble());
     SmartDashboard.putNumber(this.niceName + " target angle", this.targetAngle);
     SmartDashboard.putNumber(this.niceName + " target velocity", this.targetVelocity);
     SmartDashboard.putNumber(this.niceName + " turn current", this.turnMotor.getOutputCurrent());
-    SmartDashboard.putNumber(this.niceName + " drive current", this.driveMotor.getOutputCurrent());
+    // I think this gets the current at the motor level
+    SmartDashboard.putNumber(this.niceName + " drive current", this.driveMotorLeader.getStatorCurrent().getValueAsDouble());
   }
 
   public void setTarget(SwerveModuleState state) {
@@ -71,7 +93,7 @@ public class SwerveModule extends SubsystemBase {
   }
 
   public double getPositionMeters() {
-    return this.driveMotor.getEncoder().getPosition() * Constants.Drivetrain.WHEEL_DIAMETER * Math.PI;
+    return this.driveMotorLeader.getPosition().getValueAsDouble() * Constants.Drivetrain.WHEEL_DIAMETER * Math.PI;
   }
 
   public SwerveModulePosition getPosition() {
@@ -87,7 +109,7 @@ public class SwerveModule extends SubsystemBase {
 
   public SwerveModuleState getState() {
     return new SwerveModuleState(
-      this.driveMotor.getEncoder().getVelocity(),
+      this.driveMotorLeader.getVelocity().getValueAsDouble(),
       new Rotation2d(this.turnMotor.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle).getPosition())
     );
   }
